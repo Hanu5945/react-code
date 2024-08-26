7189622a767487128628e682d51bb1692569943a
hanu <dlgmlwn1354@gmail.com>
2024-08-26 13:06:38 +0900

    test 메세지 삭제

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
 
   return (
     <div>
-      develop test
       <h1>Response from Spring Boot API</h1>
       <button onClick={() => { setClick(!click) }}>button</button>
       <p>{message}</p> {/* API에서 받은 메시지를 출력 */}
