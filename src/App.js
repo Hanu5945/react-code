c9c83e482f5f5bffc8f085ca7126cd13ecfc7254
hanu <dlgmlwn1354@gmail.com>
2024-08-27 13:33:17 +0900

    build 삭제

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
 
   useEffect(() => {
     // Spring Boot API 호출
-    axios.get('http://localhost:13799/api/test') // http://43.201.132.91:13799/api/test 서버 실행시 주소
+    // axios.get('http://localhost:13799/api/test') // http://43.201.132.91:13799/api/test 서버 실행시 주소
+    axios.get('http://43.201.132.91:13799/api/test') //  서버 실행시 주소
       .then(response => {
         setMessage(response.data); // axios는 기본적으로 JSON 응답을 파싱합니다
       })
